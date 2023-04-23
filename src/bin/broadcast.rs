801b0f99a5068489154371233f0c9ff710e77bfc
kumarmo2 <kr.mohit6794@gmail.com>
2023-04-23 19:10:27 +0530

    3.b done i.e multi-node broadcast. TODO: check if there is a scope for optimization

--- src/bin/broadcast.rs
+++ src/bin/broadcast.rs
@@ -1,6 +1,10 @@
 #![allow(dead_code, unused_variables)]
 
-use std::{collections::HashMap, io::Write, mem};
+use std::{
+    collections::{HashMap, HashSet},
+    io::{StdoutLock, Write},
+    mem,
+};
 
 use anyhow::{bail, Context};
 use maelstrom_rs::{main_loop, Body, InitPayload, Message, Node};
@@ -9,6 +13,7 @@ struct BroadcastNode {
     node_id: String,
     msg_counter: usize,
     messages: Vec<i32>,
+    messages_set: HashSet<i32>,
     topology: HashMap<String, Vec<String>>,
 }
 
@@ -31,6 +36,71 @@ enum BroadcastPayload {
     TopologyOk,
 }
 
+impl BroadcastNode {
+    pub(crate) fn broadcast_to_neighbours(
+        &self,
+        message: i32,
+        in_msg_id: Option<usize>,
+        std_out: &mut StdoutLock,
+    ) -> anyhow::Result<()> {
+        let Some(neighbours) =  self.topology.get(&self.node_id) else {
+            bail!("no neighbour found");
+        };
+
+        for neighbour in neighbours {
+            let payload = BroadcastPayload::Broadcast { message };
+            let out = Message {
+                src: self.node_id.to_owned(),
+                dst: neighbour.to_string(),
+                body: Body {
+                    msg_id: None,
+                    in_reply_to: in_msg_id,
+                    payload,
+                },
+            };
+            serde_json::to_writer(&mut *std_out, &out)?;
+
+            std_out
+                .write_all(b"\n")
+                .context("could not write new line")?;
+        }
+
+        Ok(())
+    }
+
+    fn handle_broadcast(
+        &mut self,
+        in_msg: Message<BroadcastPayload>,
+        message: i32,
+        std_out: &mut StdoutLock,
+    ) -> anyhow::Result<()> {
+        if self.messages_set.contains(&message) {
+            return Ok(());
+        }
+
+        self.messages.push(message);
+        self.messages_set.insert(message);
+        // handlebro
+        self.broadcast_to_neighbours(message, in_msg.body.msg_id, std_out)?;
+
+        let output = Message {
+            src: in_msg.dst,
+            dst: in_msg.src,
+            body: Body {
+                msg_id: Some(self.msg_counter),
+                in_reply_to: in_msg.body.msg_id,
+                payload: BroadcastPayload::BroadcastOk,
+            },
+        };
+        serde_json::to_writer(&mut *std_out, &output)?;
+
+        std_out
+            .write_all(b"\n")
+            .context("could not write new line")?;
+        Ok(())
+    }
+}
+
 impl Node for BroadcastNode {
     type Payload = BroadcastPayload;
 
@@ -47,6 +117,7 @@ impl Node for BroadcastNode {
             node_id,
             msg_counter: 1,
             messages: Vec::new(),
+            messages_set: HashSet::new(),
             topology: HashMap::new(),
         }
     }
@@ -58,21 +129,7 @@ impl Node for BroadcastNode {
     ) -> anyhow::Result<()> {
         match in_msg.body.payload {
             BroadcastPayload::Broadcast { message } => {
-                self.messages.push(message);
-                let output = Message {
-                    src: in_msg.dst,
-                    dst: in_msg.src,
-                    body: Body {
-                        msg_id: Some(self.msg_counter),
-                        in_reply_to: in_msg.body.msg_id,
-                        payload: BroadcastPayload::BroadcastOk,
-                    },
-                };
-                serde_json::to_writer(&mut *std_out, &output)?;
-
-                std_out
-                    .write_all(b"\n")
-                    .context("could not write new line")?;
+                self.handle_broadcast(in_msg, message, std_out)?;
             }
             BroadcastPayload::BroadcastOk => {
                 self.msg_counter += 1;
@@ -87,6 +144,7 @@ impl Node for BroadcastNode {
                  * with the original vector.
                  * */
                 let messages = std::mem::replace(&mut self.messages, empty);
+                // let vec_message: Vec<_> = messages.iter().collect();
 
                 let payload = BroadcastPayload::ReadOk { messages };
                 let output = Message {
